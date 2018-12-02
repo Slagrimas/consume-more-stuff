c025da19a0474cd01e09bbe74c9d5d874b189bcf
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-12-01 17:03:19 -1000

    catergories

--- consume-more-stuff/server/db/migrations/20181201162258_create_catergories_table.js
+++ consume-more-stuff/server/db/migrations/20181201162258_create_catergories_table.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('catergories', function (table) {
+        table.increments();
+        table.string('type', 25).notNullable();
+      })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('catergories')
+
 };
