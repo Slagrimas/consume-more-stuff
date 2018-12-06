f6edcc100855c1a73f47a01e54482d4a3755134f
Jenn Ah <jennah@Jenns-Air.home>
2018-12-06 11:18:45 -1000

    updated path for allitems component

--- consume-more-stuff/src/app/pages/allItems/allitems.component.ts
+++ consume-more-stuff/src/app/pages/allItems/allitems.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { BackendService } from "../../services/backend.service";
 
-
 @Component({
-    templateUrl: './allitems.component.html',
-    styleUrls: ['./allitems.component.scss']
+  templateUrl: "./allitems.component.html",
+  styleUrls: ["./allitems.component.scss"]
 })
 export class AllItemComponent implements OnInit {
-    items: any;
+  items: any;
 
-    constructor(private backend: BackendService) {
-        this.backend.getAllItems()
-            .then((data) => {
-                this.items = data;
-            })
-            .catch((err) => {
-                console.log('this is a error', err)
-            })
-    }
+  constructor(private backend: BackendService) {
+    this.backend
+      .getAllItems()
+      .then(data => {
+        this.items = data;
+      })
+      .catch(err => {
+        console.log("this is a error", err);
+      });
+  }
 
-
-    ngOnInit() { }
-
+  ngOnInit() {}
 }
