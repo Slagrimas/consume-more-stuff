225bee11341466db13e2b4b26c2ef6ca9f17be83
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-12-06 14:34:02 -1000

    resolved login and logout process

--- consume-more-stuff/src/app/component/header/header.component.ts
+++ consume-more-stuff/src/app/component/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../../app/services/backend.service'
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { SessionService } from '../../services/session.service'
 
 
 
@@ -25,6 +26,7 @@ export class HeaderComponent implements OnInit {
     private backend: BackendService,
     private router: Router,
     private auth: AuthService,
+    private session: SessionService,
   ) { }
 
   ngOnInit() {
@@ -51,4 +53,12 @@ export class HeaderComponent implements OnInit {
   itemSearch() {
     return this.backend.searchForItem(this.search)
   }
+   
+  logout(){
+     this.session.clearSession()
+     this.isLoggedIn = false;
+     this.loginPressed = false;
+     return this.router.navigate([''])
+  }
+
 }
